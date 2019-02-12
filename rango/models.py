02e03684de68db8305c1e5a56a75c407b7720cd3
Hui Xie <2421188x@student.gla.ac.uk>
2019-02-12 14:23:46 +0000

    chapter 7

--- rango/models.py
+++ rango/models.py
@@ -24,6 +24,8 @@ class Page(models.Model):
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
 
+
+
     def __str__(self):  # For Python 2, use __unicode__ too
         return self.title
 
