23ba1042f2b9828bb3c2dffb1463a488308b8ac6
Hui Xie <2421188x@student.gla.ac.uk>
2019-02-12 13:31:52 +0000

    first commit

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
 
 def index (request):
     return HttpResponse("Rango says hey there partner!<br/> <a href='/rango/about/'>About</a>")
@@ -8,4 +10,21 @@ def about (request):
 def index(request):
    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
    return render(request, 'rango/index.html', context=context_dict)
- 
+def index(request):
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    return render(request,'rango/index.html', context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    return render(request, 'rango/category.html', context_dict)
+
+
